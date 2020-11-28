63a01138cc8d533b59faacab64feee082752797e
James Iorgovan <james.iorgovan@gmail.com>
2020-11-28 17:26:54 -0600

    Added 2 light animations for vend/lamp + new switch

---
+++ Assets/newLight/lightOnOff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightOnOff : MonoBehaviour {
+
+    public GameObject LightSwitch;
+    private bool on = false;
+
+    void OnTriggerStay(Collider plyr) {
+        if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.O) && !on)
+        {
+            LightSwitch.SetActive(true);
+            on = true;
+        }
+        else if (plyr.tag == "Player" && Input.GetKeyDown(KeyCode.O) && on)
+            {
+            LightSwitch.SetActive(false);
+            on = false;
+        }
+    }
+}
