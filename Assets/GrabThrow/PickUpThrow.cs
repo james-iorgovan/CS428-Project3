ad4b456fe1abd253d0dd1a591798714a5b4d7117
James Iorgovan <james.iorgovan@gmail.com>
2020-11-28 17:40:33 -0600

    Added pickup/throw script with ridges and colliders

---
+++ Assets/GrabThrow/PickUpThrow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpThrow : MonoBehaviour
+{
+    public Transform destination;
+
+    void OnMouseDown(){
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<Rigidbody>().useGravity = false;
+        this.transform.position = destination.position;
+        this.transform.parent = GameObject.Find("Destination").transform;
+            }
+
+    void OnMouseUp(){
+        this.transform.parent = null;
+        GetComponent<Rigidbody>().useGravity = true;
+        GetComponent<BoxCollider>().enabled = true;
+    }
+}
